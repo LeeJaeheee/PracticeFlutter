3a25ce84b82c3481e0beab327980eee34c663337
LeeJaeheee <sns3434@gmail.com>
2023-07-09 17:00:49 +0900

    메인 페이지 생성

---
+++ shazam/lib/main.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shazam',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Builder(builder: (context) {
+        DefaultTabController.of(context)?.addListener(() {
+          setState(() {});
+        });
+
+        return Scaffold(
+          body: Stack(
+            children: [
+              TabBarView(
+                children: [
+                  FirstTab(),
+                  SecondTab(),
+                  ThirdTab(),
+                ],
+              ),
+              SafeArea(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topCenter,
+                        child: TabPageSelector(
+                          color: DefaultTabController.of(context)?.index == 1
+                              ? Colors.blue[300]
+                              : Colors.grey[400],
+                          selectedColor:
+                              DefaultTabController.of(context)?.index == 1
+                                  ? Colors.white
+                                  : Colors.blue,
+                          indicatorSize: 8,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
+
+// 첫번째 페이지
+class FirstTab extends StatelessWidget {
+  const FirstTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const songs = [
+      {
+        'imageUrl': 'https://i.ytimg.com/vi/jAO0KXRdz_4/hqdefault.jpg',
+        'title': '가을밤에 든 생각',
+        'artist': '잔나비',
+      },
+      {
+        'imageUrl': 'https://i.ytimg.com/vi/jAO0KXRdz_4/hqdefault.jpg',
+        'title': '가을밤에 든 생각',
+        'artist': '잔나비',
+      },
+      {
+        'imageUrl': 'https://i.ytimg.com/vi/jAO0KXRdz_4/hqdefault.jpg',
+        'title': '가을밤에 든 생각',
+        'artist': '잔나비',
+      },
+      {
+        'imageUrl': 'https://i.ytimg.com/vi/jAO0KXRdz_4/hqdefault.jpg',
+        'title': '가을밤에 든 생각',
+        'artist': '잔나비',
+      },
+      {
+        'imageUrl': 'https://i.ytimg.com/vi/jAO0KXRdz_4/hqdefault.jpg',
+        'title': '가을밤에 든 생각',
+        'artist': '잔나비',
+      },
+      {
+        'imageUrl': 'https://i.ytimg.com/vi/jAO0KXRdz_4/hqdefault.jpg',
+        'title': '가을밤에 든 생각',
+        'artist': '잔나비',
+      },
+    ];
+
+    return Center(child: Text('첫번째 페이지'));
+  }
+}
+
+// 두번째 페이지
+class SecondTab extends StatelessWidget {
+  const SecondTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.blue[300]!, Colors.blue[900]!],
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      DefaultTabController.of(context).animateTo(0);
+                    },
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "라이브러리",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      DefaultTabController.of(context).animateTo(2);
+                    },
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.show_chart,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "차트",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Text(
+              "Shazam하려면 탭하세요",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 세번째 페이지
+class ThirdTab extends StatelessWidget {
+  const ThirdTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const chartData = {
+      'korea': [
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+      ],
+      'global': [
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+      ],
+      'newyork': [
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+      ],
+    };
+
+    return Center(child: Text('세번째 페이지'));
+  }
+}
